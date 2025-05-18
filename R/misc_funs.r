ccd009c76032c5afd08e7818056d00bf18f7b6da
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-05-18 18:19:21 +0200

    Added 'bg' argument to blank().

--- R/misc_funs.r
+++ R/misc_funs.r
@@ -1,4 +1,5 @@
-blank <- function(annotate=TRUE, ...) {
+blank <- function(annotate=TRUE, bg="white", ...) {
+   par(bg=bg)
    plot(NA, xlim=c(0,1), ylim=c(0,1), xaxt="n", yaxt="n", xlab="", ylab="", bty="n")
    dots <- list(...)
    if (annotate)
